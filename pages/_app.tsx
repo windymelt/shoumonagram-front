5d31c8b5b224f5d311be3826b1c6c07c50a75aae
Windymelt <windymelt@3qe.us>
2022-04-02 22:50:43 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
+import '../semantic/dist/semantic.min.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:8080/graphql',
+    cache: new InMemoryCache()
+  });
+  return <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 }
 
 export default MyApp
